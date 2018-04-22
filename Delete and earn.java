fd3951e73c0931405f9f74e390903450e641c270
DaweiLuo <32040865+DaweiLuo@users.noreply.github.com>
2018-04-22 17:40:04 -0400

    Add files via upload

---
+++ Delete and earn.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int deleteAndEarn(int[] nums) {
+        int[]count = new int[10001];
+        for(int n:nums){
+           count[n]+=n;
+        }
+
+        int[]dp = new int[10003];
+        for(int i =10000;i>=0;i--){
+            dp[i]=Math.max(dp[i+1],count[i]+dp[i+2]);
+        }
+        return dp[0];
+    }
+}
